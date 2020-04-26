329b1ef0cb5ba9b0d414ed4db488a8fdd9338063
I355102 <sahil.kumar@sap.com>
2020-04-26 06:20:45 +0000

    changed xsodata call to getCall

--- caremonger_service/router/routes/order.js
+++ caremonger_service/router/routes/order.js
@@ -6,7 +6,9 @@ const hdbext = require("@sap/hdbext");
 module.exports = function() {
 	var db;
 	function callProcedure(procedureName, data, db, res) {
-		data = JSON.parse(data);
+		if (procedureName !== 'caremonger.caremonger_db::p_get_orders') {
+			data = JSON.parse(data);
+		} 
 		var client = db;
 		hdbext.loadProcedure(client, null, procedureName, function (err, sp) {
 			if (err) {
@@ -16,7 +18,7 @@ module.exports = function() {
 					.send("ERROR: " + err.toString());
 				return;
 			}
-			sp(data, function (err, results) {
+			sp(data, function (err, parameterResults, tableRowsResults) {
 				if (err) {
 					res
 						.type("text/plain")
@@ -24,11 +26,31 @@ module.exports = function() {
 						.send("ERROR: " + err.message.toString());
 					return;
 				}
-				res.send(results);
+				if(!tableRowsResults) {
+					res.send(parameterResults);
+				}
+				else {
+					res.send(tableRowsResults);
+				}
 			});
 		});
 	}
-
+	
+	app.get("/getOrders", (req, res) => {
+		try {
+			db = req.db;
+			let order = {};
+			let procedureName = "caremonger.caremonger_db::p_get_orders";
+			callProcedure(procedureName, order, db, res);			
+		} catch(err) {
+			console.log(err);
+			res
+				.type("text/plain")
+				.status(400)
+				.send(err);
+		}
+	})
+	
 	app.post("/createOrder",(req,res) => {
 		try {
 			db = req.db;
